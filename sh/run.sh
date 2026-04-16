2fc69ee66fbe3d3bef8ce1f1d06df666b3a7a579
ruthsjavarone-cyber <ruthsjavarone@gmail.com>
2026-04-16 08:49:47 +0700

    Update run.sh

--- sh/run.sh
+++ sh/run.sh
@@ -17,7 +17,7 @@ CLOUD_INIT_ISO="cloud-init.iso"
 USER_NAME="user"
 USER_PASS="1234"
 ROOT_PASS="1234"
-HOST_NAME="p01020550352"
+HOST_NAME="01032886960s"
 
 # Google Sheets URL
 SHEET_URL="https://opensheet.elk.sh/1CS5OcWkBV0wBr0HPf1VmTUXp2ms8ev8zgZBtwQPbQ5Q/vi"
