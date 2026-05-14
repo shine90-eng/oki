ffbede7690535b95fadbef29ce61f89a97d9bb3e
shine90-eng <ruthsjavarone@gmail.com>
2026-05-14 20:08:15 +0700

    Update run.sh

--- sh/run.sh
+++ sh/run.sh
@@ -17,7 +17,7 @@ CLOUD_INIT_ISO="cloud-init.iso"
 USER_NAME="user"
 USER_PASS="1234"
 ROOT_PASS="1234"
-HOST_NAME="nguyenlephuong937"
+HOST_NAME="le214667"
 
 # Google Sheets URL
 SHEET_URL="https://opensheet.elk.sh/1CS5OcWkBV0wBr0HPf1VmTUXp2ms8ev8zgZBtwQPbQ5Q/vi"
